78051b2ff075ced7276d0cc94608a2e65e7c3625
diome <diomelga99@gmail.com>
2024-10-16 21:47:47 -0300

    Primer commit Proyecto de recetas

--- app/src/main/java/com/diegomelgarejo/recetascocina/MainActivity.kt
+++ app/src/main/java/com/diegomelgarejo/recetascocina/MainActivity.kt
@@ -3,7 +3,6 @@ package com.diegomelgarejo.recetascocina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -29,7 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.diegomelgarejo.recetascocina.ui.theme.RecetasCocinaTheme
 
 class MainActivity : ComponentActivity() {
